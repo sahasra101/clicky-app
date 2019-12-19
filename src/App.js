d25f430dada5fdcc12b358db9cfac670b62233ec
sahasra101 <ajaykiri7@gmail.com>
2019-12-19 00:35:40 -0500

    reformatted image card and renamed parts of code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import FriendCard from "./components/FriendCard";
+import ImageCard from "./components/FriendCard";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
 import friends from "./friends.json";
@@ -10,26 +10,34 @@ class App extends Component {
     friends
   };
 
-  removeFriend = id => {
-    // Filter this.state.friends for friends with an id not equal to the id being removed
-    const friends = this.state.friends.filter(friend => friend.id !== id);
-    // Set this.state.friends equal to the new friends array
-    this.setState({ friends });
+  imageClick = id => {
+
   };
+
+  cardShuffle(){
+
+  }
 
   // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
     return (
       <Wrapper>
-        <Title>Friends List</Title>
+        <nav className="navbar sticky-top navbar-light bg-light">
+          <ul>
+            <li className="brand"><a href="/">Clicky Game</a></li>
+            <li>Click an image to begin!</li>
+            <li>Score: 0 | Top Score: 0</li>
+            </ul>
+            </nav>
+        <Title>Superfriends List</Title>
         {this.state.friends.map(friend => (
-          <FriendCard
+          <ImageCard
             removeFriend={this.removeFriend}
             id={friend.id}
             key={friend.id}
             name={friend.name}
             image={friend.image}
-            occupation={friend.occupation}
+            alias={friend.alias}
             location={friend.location}
           />
         ))}
--- src/components/FriendCard/index.js
+++ src/components/FriendCard/index.js
@@ -1,30 +1,13 @@
 import React from "react";
 import "./style.css";
 
-function FriendCard(props) {
+function ImageCard(props) {
   return (
-    <div className="card">
-      <div className="img-container">
-        <img alt={props.name} src={props.image} />
+    <div className="card" onClick={() => props.cardShuffle()}>
+      <div onClick={() => props.imageClick(props.id)} className="img-container">
+        <img id={props.id} alt={props.name} src={props.image} />
       </div>
-      <div className="content">
-        <ul>
-          <li>
-            <strong>Name:</strong> {props.name}
-          </li>
-          <li>
-            <strong>Occupation:</strong> {props.occupation}
-          </li>
-          <li>
-            <strong>Location:</strong> {props.location}
-          </li>
-        </ul>
-      </div>
-      <span onClick={() => props.removeFriend(props.id)} className="remove">
-        𝘅
-      </span>
     </div>
   );
 }
-
-export default FriendCard;
+export default ImageCard;
